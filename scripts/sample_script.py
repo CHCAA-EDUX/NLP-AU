772cc0e9d58977dfe05c830f180816028e7c6fa0
rbroc <rbrrcc@gmail.com>
2023-09-11 14:57:58 +0200

    lint sample_script. ø

--- scripts/sample_script.py
+++ scripts/sample_script.py
@@ -1,4 +1,3 @@
-from collections import Counter
 import argparse
 
 
@@ -7,21 +6,25 @@ parser.add_argument("--start", type=int, default=0,
                     help="start index for slicing")
 parser.add_argument("--end", type=int, default=0, 
                     help="end index for slicing")
+parser.add_argument("--filename", type=str, 
+                    help="filename for the input text")
 
 
-def string_slicer(start_idx=0, end_idx=10):
+def string_slicer(filename, start_idx=0, end_idx=10):
     '''
     Returns a slice of of war and peace, indexed by character position
 
             Parameters:
+                    filename (str): Filename where input text is found
                     start_idx (int): Start index
                     end_idx (int): End index
 
             Returns:
-                    binary_sum (str): Binary string of the sum of a and b
+                    sliced_text (str): Text using input indices
     '''
-    text = open('../data/book-war-and-peace.txt').read()
-    return text[start_idx:end_idx]
+    text = open(f'../data/{filename}').read()
+    sliced_text = text[start_idx:end_idx]
+    return sliced_text
 
 
 if __name__=='__main__':
